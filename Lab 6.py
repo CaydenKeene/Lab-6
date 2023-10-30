b2f39cde80757a94f892ee6de0964c63d36440e7
CaydenKeene <134308637+CaydenKeene@users.noreply.github.com>
2023-10-30 10:54:53 -0400

    added lab 6 with complete encode and main function

---
+++ Lab 6.py
@@ -0,0 +1,24 @@
+def encoder(password):
+    encoded_password = ""
+    for i in password:
+        i = int(i)
+        i += 3
+        if i > 9:
+            i -= 10
+        encoded_password += str(i)
+    return encoded_password
+
+
+if __name__ == '__main__':
+    while True:
+        print("Menu\n-------------\n1. Encode\n2. Decode\n3. Quit\n")
+        user_input = int(input("Please enter an option: "))
+        if user_input == 1:
+            encoded_password = encoder(input("Please enter your password to encode: "))
+            print("Your password has been encoded and stored!\n")
+
+        elif user_input == 2:
+            print(f"The encoded password is {encoded_password}, and the original password is {decoded_password}.")
+
+        elif user_input == 3:
+            break
